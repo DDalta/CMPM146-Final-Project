d312a9b718ce93755bd90278b523f12e611e63ad
Justin Park <jutpark@ucsc.edu>
2024-03-17 05:56:03 -0700

    Adding Non-working enemy in FOV Range with comments to complete. Need to figure out player 2d vision collider

---
+++ Assets/AgentBT/CheckEnemyInFOVRange.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BehaviorTree;
+
+public class CheckEnemyInFOVRange : Node
+{
+    private static int _enemyLayerMask = LayerMask.NameToLayer("Enemy");
+    private static int _playerLayerMask = LayerMask.NameToLayer("Player");
+    private Transform _transform;
+    private Animator _animator;
+
+    public CheckEnemyInFOVRange(Transform transform)
+    {
+        _transform = transform;
+        _animator = transform.GetComponent<Animator>();
+    }
+
+    public override NodeState Evaluate()
+    {
+        object t = GetData("target");
+        if (t == null)
+        {
+            Collider[] colliders = Collider2D.IsTouchingLayers(_enemyLayerMask, _playerLayerMask);//check if enemy layermask and the player's vision 2d collider collide. If they do then we return the state as
+            //having a target to run away from and sets walking to be true as a reminder to RUN AWAY
+            
+
+
+            if (colliders.Length > 0)
+            {
+                parent.parent.SetData("target", colliders[0].transform);
+                _animator.SetBool("Walking", true);
+                state = NodeState.SUCCESS;
+                return state;
+            }
+
+            state = NodeState.FAILURE;
+            return state;
+        }
+
+        state = NodeState.SUCCESS;
+        return state;
+    }
+
+}
